9e7e0d3bf253e93e37efe41d5d5f531341bdf62a
natalinogueira <natali.nogueira01@gmail.com>
2020-05-18 20:29:51 -0300

    change app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,25 +5,19 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
-<<<<<<< HEAD
 app.get('/jupiter', function (req, res) {
   res.send('Hello Jupiter!\n');
 });
 
-=======
->>>>>>> 02f923411332b0f0db2c66b48b4860a784b72a42
 app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
-<<<<<<< HEAD
 app.get('/neptune', function (req, res) {
   res.send('Hello Neptune!\n');
 });
-=======
->>>>>>> 02f923411332b0f0db2c66b48b4860a784b72a42
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(8088, function () {
+  console.log('Example app listening on port 8088!');
 });
 
